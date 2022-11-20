c45b74fc877992dd93474690edf0c9f229e1e464
Nenye <chinenyediei@gmail.com>
2022-11-20 07:06:27 -0600

    double cheked sc.next int output

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,6 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessingGame {
     public GuessingGame() {
         System.out.println("Hello there, what's your name?");
+        System.out.print("Please type in your name : ");
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        //test
+        System.out.println(name);
+        Random rand = new Random();
+        //create instance
+        int number = sc.nextInt(100);
+        //call method nextInt and pass
+
 
     }
 }
