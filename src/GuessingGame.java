f7c7def78ba0b8acd53c4617c49932fd2f66bb8f
Nenye <chinenyediei@gmail.com>
2022-11-20 15:33:49 -0600

    testing works for name, random number, and accepting user guess. now need to compare user guess and say, higher/lower or end game

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,7 +1,12 @@
+import com.sun.tools.javac.Main;
+
 import java.util.Random;
 import java.util.Scanner;
 
+
+
 public class GuessingGame {
+
     public GuessingGame() {
         System.out.println("Hello there, what's your name?");
         System.out.print("Please type in your name : ");
@@ -9,12 +14,18 @@ public class GuessingGame {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         //test
-        System.out.println(name);
         Random rand = new Random();
         //create instance
-        int number = sc.nextInt(100);
-        //call method nextInt and pass
-
+        System.out.println(name + " we're going to play a guessing name, I have a number between 1-100." + name + " " +
+                        "can you guess what it is!");
+        System.out.print("Try to guess my number :");
+        int userGuess = sc.nextInt();
+        int number = rand.nextInt(100);
+        //count guesses
+        //when guess = number; game over
+        System.out.println(number);
+        System.out.println(userGuess);
+        //check condition
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class Main {
+public class Main extends GuessingGame{
     public static void main(String[] args) {
         System.out.println("Hello world!");
+       new GuessingGame();
+
     }
 }
