1f7c98dce99631f8fa1383cd505989cf421eafaf
Nenye <chinenyediei@gmail.com>
2022-11-20 17:44:38 -0600

    asks user if they want to play and saves name and players choice

--- src/MarsExpedition.java
+++ src/MarsExpedition.java
@@ -10,7 +10,19 @@ public class MarsExpedition {
         System.out.println("...");
         System.out.println("Ready...");
 
-        //get user name
+        System.out.println("Hello what's your name?");
+        String name = sc.nextLine();
+        System.out.print("Hi," + name + " Welcome to the Expediton prep program. Are you ready to head out into the new world? Type Y or N? ");
+        String input = sc.nextLine();
+
+        //check condition
+        if(input.equalsIgnoreCase("Y")) {
+            System.out.println(name + " I knew you would say that. You are a team leader for a reason!");
+        }
+        else {
+            System.out.println("Too bad you aren't a team leader. You have to go.");
+        }
+
         //greet user and ask if they wanna play w y/n
         //save input
         //if no, send message
