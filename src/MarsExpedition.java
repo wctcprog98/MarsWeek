1964f76abe6e20e1f0f11ee9de10c8d86e8302fd
Nenye <chinenyediei@gmail.com>
2022-11-20 18:39:25 -0600

    prompts works all the way to responding with snack choice of user

--- src/MarsExpedition.java
+++ src/MarsExpedition.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MarsExpedition {
@@ -18,16 +19,32 @@ public class MarsExpedition {
         //check condition
         if(input.equalsIgnoreCase("Y")) {
             System.out.println(name + " I knew you would say that. You are a team leader for a reason!");
+            System.out.println("How many people would like on your team?");
+
         }
         else {
             System.out.println("Too bad you aren't a team leader. You have to go.");
         }
-
-        //greet user and ask if they wanna play w y/n
-        //save input
-        //if no, send message
-        //if yes continue
         //ask user how many people are on their team
+        int teamSize = sc.nextInt();
+        System.out.println(teamSize);
 
+
+            if (teamSize > 3) {
+                System.out.println("That's way too many people. We can only send 3 total members");
+                teamSize = 3;
+            } else if (teamSize < 3) {
+                System.out.println("That's not enough people to play, we need a team of 3 total members");
+                teamSize = 3;
+            } else if (teamSize == 3) {
+                System.out.println("That's a perfect sized team. Good job");
+            }
+
+        sc.nextLine();
+        //tell user they are logged on and they one snack
+        System.out.println(name + ", you are allowed to bring one snack. What would you like to bring?");
+        String snack = sc.nextLine();
+        System.out.println("Nice choice " + name + ", you will be bringing a " + snack + " with you.");
+        //
     }
 }
